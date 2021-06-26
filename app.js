d4511bb9c6160934451f1b201609f5cae9fe8d75
Abdullahi <abdullahiadamu600@gmail.com>
2021-06-26 17:50:56 +0100

    Full server with output

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ function rqListner(req, res){
 }
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    //process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html');
+    res.write('<head><title>My First Page </title></head>');
+    res.write('<body><h1>Hello from my Node.js Server');
+    res.write('</html>');
+    res.end();
 });
 
-server.listen(3000);
+server.listen(3000);  
