da6ea90d630a4f7b2361e6da66b86531d3c2ad1c
Abdullahi <abdullahiadamu600@gmail.com>
2021-08-22 15:14:22 +0100

    installed nodemon package

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
 const http = require('http');
 
-function rqListner(req, res){
+const routes = require('./routes');
 
-}
-
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    //process.exit();
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html');
-    res.write('<head><title>My First Page </title></head>');
-    res.write('<body><h1>Hello from my Node.js Server');
-    res.write('</html>');
-    res.end();
-});
-
+const server = http.createServer(routes);
 server.listen(3000);  
